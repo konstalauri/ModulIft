8bfa06215d571b40dab931dfaed72f1b4934dba7
Jin Guanghe <jinthreek89@gmail.com>
2015-02-04 07:59:32 +0100

    Initial Commit

---
+++ Modulift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Modulift
+//
+//  Created by Jordan Augustine on 04/02/15.
+//  Copyright (c) 2015 Jordan Augustine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Modulift/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Modulift
+//
+//  Created by Jordan Augustine on 04/02/15.
+//  Copyright (c) 2015 Jordan Augustine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
